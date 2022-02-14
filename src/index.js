0b7c429c1848a2f3b79c0f6035180f73bf226a67
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-02-14 14:16:17 -0400

    feat(build): change build folder by ui folder, requirement of sbbw

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'react-clock/dist/Clock.css';
+
+const App = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(new Date()), 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [time]);
+
+    return (
+        <div>
+            <Clock className="clock" value={time}/>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
